95e566972f5c2facddffec058d8d9e41e806ea32
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-18 19:48:15 +0800

    feat: fully CRUD api for User class now works

---
+++ fareshare-backend/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    has_secure_password
+  
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :user_name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
+
+end
+  
