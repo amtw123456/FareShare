03a263d7c3e6b7ea7c2cbe1a202e69a8d0cf447c
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-19 14:55:24 +0800

    feat: added new pages

---
+++ fareshare-frontend/src/app/profile/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+
+interface User {
+    email: string;
+    password: string;
+}
+
+const Home = () => {
+
+
+    return (
+        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+            red
+        </div>
+    );
+};
+
+export default Home;
