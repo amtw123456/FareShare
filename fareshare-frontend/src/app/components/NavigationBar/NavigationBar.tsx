aea968fd2ffee025a635f800cfaa664b017a0e0d
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-20 18:32:29 +0800

    feat: Navbar Item is now focused when in that current page

--- fareshare-frontend/src/app/components/NavigationBar/NavigationBar.tsx
+++ fareshare-frontend/src/app/components/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,10 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import { AcmeLogo } from "./AcmeLogo";
+import { usePathname } from 'next/navigation';
 
 export default function NavigationBar() {
+    const pathname = usePathname(); // Get the current pathname directly from the hook
+
     return (
         <Navbar isBordered>
             <NavbarBrand>
@@ -9,13 +12,13 @@ export default function NavigationBar() {
                 <p className="font-bold text-inherit">ACME</p>
             </NavbarBrand>
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarItem isActive>
-                    <Link href="/home" aria-current="page">
+                <NavbarItem isActive={pathname === "/home"}>
+                    <Link href="/home" aria-current={pathname === "/home" ? "page" : undefined}>
                         Feed
                     </Link>
                 </NavbarItem>
-                <NavbarItem isActive>
-                    <Link href="/profile" aria-current="page">
+                <NavbarItem isActive={pathname === "/profile"}>
+                    <Link href="/profile" aria-current={pathname === "/profile" ? "page" : undefined}>
                         Profile
                     </Link>
                 </NavbarItem>
