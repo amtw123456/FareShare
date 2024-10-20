ae8020f396ef3c727f1b53ee13bbd280e9ca0021
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-10-20 18:36:31 +0800

    feat: integrated navigation bar on home and profile page

--- fareshare-frontend/src/app/home/page.tsx
+++ fareshare-frontend/src/app/home/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState, ChangeEvent, FormEvent } from 'react';
 import axios from 'axios';
+import NavigationBar from '../components/NavigationBar/NavigationBar';
+import CreateTransactionModal from '../components/CreateTransactionModel/CreateTransactionModal';
 
 interface User {
     email: string;
@@ -13,36 +15,14 @@ const Home = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    };
-
-    const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // Prevent the default form submission behavior
-        console.log(user)
-        try {
-            const response = await axios.post('http://localhost:3000/login', {
-                user: {  // Wrap the email and password in a user object
-                    email: user.email,
-                    password: user.password,
-                }
-            });
-
-            // Handle successful login here (e.g., save token, redirect user)
-            console.log('Login successful:', response.data);
-        } catch (err) {
-            if (axios.isAxiosError(err) && err.response) {
-                setError(err.response.data.error || 'Login failed');
-            } else {
-                setError('An unexpected error occurred');
-            }
-        }
-    };
-
     return (
-        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-            Home
-        </div>
+        <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900">
+            <NavigationBar />
+            <div className='flex justify-center'>
+                <CreateTransactionModal />
+                home
+            </div>
+        </div >
     );
 };
 
--- fareshare-frontend/src/app/profile/page.tsx
+++ fareshare-frontend/src/app/profile/page.tsx
@@ -4,6 +4,7 @@ import { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import CreateTransactionModal from "../components/CreateTransactionModel/CreateTransactionModal"
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
+import NavigationBar from '../components/NavigationBar/NavigationBar';
 
 interface User {
     email: string;
@@ -14,16 +15,20 @@ const Profile = () => {
     const { token, userId, userEmail, tokenExpiry, setToken, setUserId, setUserEmail, setTokenExpiry } = useAuth();
 
     useEffect(() => {
-        console.log(token)
-        console.log(userId)
-        console.log(userEmail)
-        console.log(tokenExpiry)
+        // console.log(token)
+        // console.log(userId)
+        // console.log(userEmail)
+        // console.log(tokenExpiry)
     }, [token, userId, userEmail, tokenExpiry]);
 
     return (
-        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-            <CreateTransactionModal />
-        </div>
+        <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900">
+            <NavigationBar />
+            <div className='flex justify-center'>
+                <CreateTransactionModal />
+                Profile
+            </div>
+        </div >
     );
 };
 
